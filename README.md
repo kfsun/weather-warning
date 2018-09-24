80645a50b1b35734fc295f5bd892691aaed61a50
kafai <kfsworks@gmail.com>
2018-09-24 09:49:50 +0800

    tidy up

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# weather-warning-hko
+# weather-warning
 
-Test
+This is a simple program to pull weather warning rss from Hong Kong Observatory.
+And the warning will be sent to the notification daemon in Linux.
+
+![Sample screen shot](https://github.com/kfsworks/weather-warning/tree/master/static/sample.png)
--- main.go
+++ main.go
@@ -2,11 +2,51 @@ package main
 
 import (
 	"fmt"
-	"github.com/kfsworks/weather-warning/processor"
+	//"github.com/kfsworks/weather-warning/processor"
+	"github.com/kfsworks/weather-warning/fetcher"
+	"github.com/kfsworks/weather-warning/warning"
+	"github.com/mqu/go-notify"
+	"log"
+	"time"
+	//    "os"
 )
 
+func sendNotification(c chan warning.WeatherWarning) {
+	var oldWarning warning.WeatherWarning
+
+	for {
+		warning := <-c
+
+		if warning.IsNoWarning() {
+			time.Sleep(time.Second * 1)
+			continue
+		}
+
+		if oldWarning.PubDate.IsZero() {
+			log.Println("save old")
+			oldWarning = warning
+		} else {
+			difference := warning.PubDate.Sub(oldWarning.PubDate)
+			if difference.Nanoseconds() == 0 {
+				log.Println("same and skip")
+				continue
+			}
+		}
+
+		notify.Init("Weather Warning")
+		notification := notify.NotificationNew(warning.Title, warning.Description, "dialog-information")
+		notification.Show()
+
+		time.Sleep(time.Second * 1)
+	}
+}
+
 func main() {
-	processor.Process()
+	var c chan warning.WeatherWarning = make(chan warning.WeatherWarning, 5)
+
+	go sendNotification(c)
+	go fetcher.Fetch(c)
+	//processor.Process()
 
 	var input string
 	fmt.Scanln(&input)
